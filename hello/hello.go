91aa32e3df60e951257d5f83644d762721451e66
User <auh202001@yahoo.com>
2018-03-11 15:38:09 -0400

    updates.

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+// https://github.com/GoClipse/goclipse/blob/latest/documentation/UserGuide.md#user-guide
+
+import (
+	"fmt"
+	"github.com/troodes/sandbox/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf("hello, world.\n")
+    fmt.Printf(stringutil.Reverse("hello, world\n"))
 }
