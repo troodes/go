5a0bfe3a05e8d1d2b470cd9a4ece9f4448aac923
Rick <ricktrudeau1@gmail.com>
2018-03-14 20:27:21 -0400

    updates.

--- hello/hello.go
+++ hello/hello.go
@@ -41,10 +41,10 @@ func main() {
 	data = getDataFromUrl(url)
 	fmt.Println(data)
 	
-	url = "https://www.worldtradingdata.com/api/v1/history?api_token=" + myConfig.WtdKey + "&symbol=" + ticker
-	fmt.Println(url)
-	data = getDataFromUrl(url)
-	fmt.Println(data)
+	//url = "https://www.worldtradingdata.com/api/v1/history?api_token=" + myConfig.WtdKey + "&symbol=" + ticker
+	//fmt.Println(url)
+	//data = getDataFromUrl(url)
+	//fmt.Println(data)
 	
 
 	fmt.Println("Terminating the application...")
